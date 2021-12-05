d3858ee0b72ca2a30fc6fecc6916eb0eb9a3a55a
mva18 <mullervanas@gmail.com>
2021-12-05 20:01:23 +0200

    fixed docker image's to public available images and test

--- .docker/mysql/database.sql
+++ .docker/mysql/database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Dec 05, 2021 at 04:25 PM
--- Server version: 5.7.31
--- PHP Version: 7.4.9
+-- Generation Time: Dec 05, 2021 at 05:55 PM
+-- Server version: 8.0.27
+-- PHP Version: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,10 +30,11 @@ USE `match_database`;
 --
 
 CREATE TABLE `matches` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `profile_id` int(11) NOT NULL,
-  `pref` int(1) NOT NULL
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `profile_id` int NOT NULL,
+  `pref` int NOT NULL,
+  `matched` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -43,15 +44,14 @@ CREATE TABLE `matches` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `gender` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `age` int(11) NOT NULL,
+  `age` int NOT NULL,
   `token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `token_expire` date DEFAULT NULL,
-  `matches` int(11) DEFAULT NULL
+  `token_expire` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -69,24 +69,3 @@ ALTER TABLE `matches`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `matches`
---
-ALTER TABLE `matches`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=121;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
